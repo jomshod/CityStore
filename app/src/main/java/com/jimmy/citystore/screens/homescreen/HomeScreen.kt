46729998b4d6c1d1bf28d8093560814f96e66c2e
jamshaid <aeoikhlaspurw@gmail.com>
2024-11-09 21:09:39 +0500

    added the top bar home

--- app/src/main/java/com/jimmy/citystore/screens/homescreen/HomeScreen.kt
+++ app/src/main/java/com/jimmy/citystore/screens/homescreen/HomeScreen.kt
@@ -4,11 +4,16 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
@@ -17,7 +22,7 @@ import com.jimmy.citystore.ui.theme.CityStoreTheme
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier, navController: NavController) {
-    Scaffold { paddingValues ->
+    Scaffold(topBar = { HomeTopBar() }) { paddingValues ->
         StoreCard(
             modifier = modifier.padding(paddingValues),
             navController
@@ -28,7 +33,7 @@ fun HomeScreen(modifier: Modifier = Modifier, navController: NavController) {
 @Composable
 fun StoreCard(modifier: Modifier, navController: NavController) {
     Card(
-        modifier = Modifier
+        modifier = modifier
             .clickable { navController.navigate(AppScreens.GeneralStore.route) }
             .fillMaxWidth(),
     ) {
@@ -36,6 +41,15 @@ fun StoreCard(modifier: Modifier, navController: NavController) {
         Text("GeneralStore")
         Text("GeneralStore")
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeTopBar() {
+    TopAppBar(
+        title = { Text("CityStore", modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center) },
+        colors = TopAppBarDefaults.topAppBarColors(containerColor = MaterialTheme.colorScheme.primary)
+    )
 }
 
 @Preview(showBackground = true, showSystemUi = true)
